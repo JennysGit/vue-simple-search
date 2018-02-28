f152c41c79b51e8bd2c3220beeba2e7fd9db40df
zhangjing <1098739658@qq.com>
2018-02-28 14:29:54 +0800

    search result pagination

---
+++ src/components/pagination/pagination.vue
@@ -0,0 +1,56 @@
+<!-- don' use now -->
+<template>
+	<nav aria-label="Page navigation" class="text-center">
+      <ul class="pagination">
+        <li>
+          <a href="javascript:void(0)" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li><a href="javascript:void(0)">1</a></li>
+        <li><a href="javascript:void(0)">2</a></li>
+        <li><a href="javascript:void(0)">3</a></li>
+        <li><a href="javascript:void(0)">4</a></li>
+        <li><a href="javascript:void(0)">5</a></li>
+        <li>
+          <a href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+</template>
+<script type="text/javascript">
+	export default {
+		name: 'pagination',
+		props: {
+			pageData： {
+				type: Array,
+				default: []
+			}
+		},
+		data () {
+			return {
+				pageSize: 5, // 每页显示条数
+				pageCount: 0 // 总页数
+				pageCurrent:1, // 当前页
+				totalCount: 0, //总页数
+			}
+		},
+		computed: {
+			totalPage() {
+				return Math.ceil(this.pageNum / this.limit)
+			},
+			currentPage() {
+				return Math.ceil(this.offset / this.limit) + 1
+			}
+		},
+		methods: {
+			prev () {
+
+			},
+
+		}
+		
+	}
+</script>
--- src/components/search.vue
+++ src/components/search.vue
@@ -11,36 +11,54 @@
           <input id="keyword" type="text" class="form-control" placeholder="标签"  v-model="searchData.tags"/>
         </div>
         <div class="col-sm-3 form-group">
-          <label for="suit">功效</label>
-          <input id="suit" type="text" class="form-control" placeholder="功效" v-model="searchData.suit"/>
+          <label for="common">常识</label>
+          <input id="common" type="text" class="form-control" placeholder="常识" v-model="searchData.common"/>
         </div>
         <div class="col-sm-3 form-group">
           <button class="btn btn-primary search-btn" @click="search()" :disabled="isSearching">搜索</button>
         </div>
       </div>
     </form>
-    <table class="table table-hover table-striped search-result-table" v-show="foodList.length > 0 && !isSearching">
+    <table class="table table-hover table-striped search-result-table" v-show="page.data.length > 0 && !isSearching">
       <thead>
         <tr>
           <th width="20%">菜名</th>
           <th width="30%">标签</th>
-          <th width="40%">功效</th>
+          <th width="40%">常识</th>
           <th width="10%">操作</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="food in foodList">
+        <tr v-for="food in page.data">
           <td>{{food.title}}</td>
           <td class="tags"><button v-for="tag in food.tags" class="btn" :class="{'btn-success': tag.type == 2}">{{ tag.text }}</button></td>
-          <td><span v-for="suit in food.suits">{{ suit }}</span></td>
-          <td><a href="javascript:void(0)" ng-click="showDetail()">详情</a></td>
+          <td><span>{{ food.common.txt }}</span></td>
+          <td><a href="javascript:void(0)" :click="showDetail()">详情</a></td>
         </tr>
       </tbody>
     </table>
+    <nav aria-label="Page navigation" class="text-center" v-show="page.numArray.length > 0 && !isSearching">
+      <ul class="pagination">
+        <li :class="{'disabled': page.curIndex == 0}">
+          <a href="javascript:void(0)" aria-label="Previous"  @click="goPageIndex(page.curIndex - 1)">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        <li v-for="pageNum in page.numArray" :class="{'active': page.curIndex == pageNum - 1}">
+          <a href="javascript:void(0)" @click="goPageIndex(pageNum - 1)">
+          <span>{{ pageNum }}</span>
+          </a>
+        </li>
+        <li :class="{'disabled': page.curIndex == page.total - 1}">
+          <a href="javascript:void(0)" aria-label="Next"  @click="goPageIndex(page.curIndex + 1)">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
     <div class="in-searching text-center" v-show="isSearching">
       <img :src="loadingImgUrl" alt="">
-    </div>
-    
+    </div> 
   </div>
 </template>
 
@@ -51,10 +69,17 @@ export default {
   name: 'search',
   data () {
     return {
-      searchData: {title: '', tags: '', suit: ''},
+      searchData: {title: '', tags: '', common: ''},
       foodList: [],
       isSearching: false,
-      loadingImgUrl: require('../assets/loading.gif')
+      loadingImgUrl: require('../assets/loading.gif'),
+      page: {
+        total: 0,
+        curIndex: 0,
+        size: 5,
+        data: [],
+        numArray: []
+      }
     }
   },
   methods: {
@@ -70,8 +95,8 @@ export default {
         searchParams.tags = searchData.tags // TODO: parse as array.
       }
 
-      if (searchData.suit && searchData.suit.trim()) {
-        searchParams.suit = searchData.suit
+      if (searchData.common && searchData.common.trim()) {
+        searchParams.common = searchData.common
       }
 
       if (Object.keys(searchParams).length === 0) {
@@ -84,14 +109,39 @@ export default {
         .then((res) => {
           if (res.data.data) {
             this.foodList = res.data.data
+            this._pageData(this.foodList)
             this.isSearching = false
-            console.log(res.data)
+            console.log("foodList", this.foodList)
           }
         })
         .catch((err) => {
           console.log(err)
           this.isSearching = false
         })
+    },
+    _pageData (data) {
+      this.page.numArray = []
+      this.page.total = Math.ceil(data.length / this.page.size)
+      for(let i = 1; i <= this.page.total; i++){
+        this.page.numArray.push(i)
+      }
+      if(data.length == 0){
+        this.page.data = []
+        return
+      }
+      this.goPageIndex(0)
+    },
+    goPageIndex (index) { 
+      console.log("this.page.index",index)
+      if(index >= 0 && index <= this.page.total - 1) {
+        let startIndex = index * this.page.size
+        let endIndex = startIndex + this.page.size
+        this.page.data = this.foodList.slice(startIndex, endIndex)
+        this.page.curIndex = index
+      }
+    },
+    showDetail() {
+
     }
   }
 }
